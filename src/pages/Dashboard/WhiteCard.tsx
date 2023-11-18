1d0f1acbd6e8d1d9196bd27b8aadae6ed75f7b44
Aderemi Dare <fitzgeraldkachi@gmail.com>
2023-11-18 13:47:40 +0100

    latestt hit

---
+++ src/pages/Dashboard/WhiteCard.tsx
@@ -0,0 +1,227 @@
+import { SVGProps } from "react";
+type Props = {
+  index?: boolean;
+} & SVGProps<SVGSVGElement>;
+
+const WhiteCard = ({ index, ...props }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={258}
+    height={184}
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <rect width={257.128} height={184} fill="#fff" rx={9.436} />
+      <rect
+        width={215.846}
+        height={173.385}
+        x={21.82}
+        y={21.82}
+        stroke="#D3D3D4"
+        strokeWidth={1.179}
+        rx={4.128}
+      />
+      <rect
+        width={217.026}
+        height={161.59}
+        x={21.231}
+        y={21.23}
+        fill="#D9D9D9"
+        opacity={0.3}
+        rx={4.718}
+      />
+      <circle cx={30.667} cy={29.487} r={2.359} fill="#EF847E" />
+      <circle cx={38.923} cy={29.487} r={2.359} fill="#EFC558" />
+      <circle cx={47.179} cy={29.487} r={2.359} fill="#5DD170" />
+      <circle cx={31.846} cy={48.359} r={4.718} fill="#D3D3D4" />
+      <circle cx={31.846} cy={48.359} r={4.718} fill="#D3D3D4" />
+      <rect
+        width={34.205}
+        height={5.897}
+        x={41.282}
+        y={44.82}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={34.205}
+        height={5.897}
+        x={41.282}
+        y={44.82}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={48.359}
+        height={3.538}
+        x={27.128}
+        y={62.513}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={89.051} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={75.487}
+        height={3.538}
+        x={115.59}
+        y={87.282}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={98.487} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={103.795}
+        height={3.538}
+        x={115.59}
+        y={96.718}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={107.923} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={103.795}
+        height={3.538}
+        x={115.59}
+        y={106.154}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={122.077} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={103.795}
+        height={3.538}
+        x={115.59}
+        y={120.308}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={131.513} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={103.795}
+        height={3.538}
+        x={115.59}
+        y={129.744}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <circle cx={105.564} cy={145.666} r={2.949} fill="#D3D3D4" />
+      <rect
+        width={103.795}
+        height={3.538}
+        x={115.59}
+        y={143.897}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <rect
+        width={48.359}
+        height={8.256}
+        x={27.128}
+        y={71.948}
+        fill="#1D8EE6"
+        rx={2.949}
+      />
+      <rect
+        width={24.769}
+        height={8.256}
+        x={194.615}
+        y={156.872}
+        fill="#1D8EE6"
+        rx={2.949}
+      />
+      <rect
+        width={48.359}
+        height={3.538}
+        x={27.128}
+        y={86.103}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <rect
+        width={48.359}
+        height={3.538}
+        x={27.128}
+        y={96.718}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <rect
+        width={43.641}
+        height={3.538}
+        x={27.128}
+        y={106.153}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <rect
+        width={23.59}
+        height={3.538}
+        x={27.128}
+        y={120.308}
+        fill="#D9D9D9"
+        rx={1.769}
+      />
+      <path stroke="#EAEAEB" strokeWidth={0.354} d="M85.1 34.205V182.82" />
+      <rect
+        width={114.41}
+        height={14.154}
+        x={102.615}
+        y={38.923}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={88.462}
+        height={5.897}
+        x={102.615}
+        y={57.077}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={17.692}
+        height={5.897}
+        x={193.077}
+        y={57.077}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={48.359}
+        height={5.897}
+        x={102.615}
+        y={66.975}
+        fill="#D9D9D9"
+        rx={2.949}
+      />
+      <rect
+        width={28.308}
+        height={28.308}
+        x={221.744}
+        y={148.615}
+        fill="#007AF4"
+        rx={14.154}
+      />
+      <path
+        fill="#fff"
+        d="M234.5 166.599c-.264 0-.529-.1-.73-.302l-2.799-2.799a1.031 1.031 0 0 1 1.459-1.46l2.07 2.07 4.869-4.869a1.031 1.031 0 1 1 1.459 1.46l-5.598 5.598a1.028 1.028 0 0 1-.73.302Z"
+      />
+    </g>
+    <rect
+      width={254.769}
+      height={181.641}
+      x={1.179}
+      y={1.179}
+      stroke="#1D8EE6"
+      strokeWidth={2.359}
+      rx={8.256}
+    />
+    <defs>
+      <clipPath id="a">
+        <rect width={257.128} height={184} fill="#fff" rx={9.436} />
+      </clipPath>
+    </defs>
+  </svg>
+)
+export default WhiteCard
--- src/pages/Hiring/Hiring.tsx
+++ src/pages/Hiring/Hiring.tsx
@@ -28,27 +28,13 @@ const Hiring = () => {
             label: 'Active',
             content: (
               <>
-                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
-                  <div>
-                    <img src="/cardi.svg" alt="" />
-                  </div>
-
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                  </a>
-                </div>
-
-
-                <a href="#" className="flex flex-col items-center p-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-sm ">
-                  <img className="object-cover w-full" src="/cardi.svg" alt="" />
-                  <div className="flex flex-col justify-between p-4 leading-normal">
+                <a href="#" className="flex flex-col gap-2 items p-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md">
+                  <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/cardi.svg" alt="" />
+                  <div className="flex flex-col  p-4 leading-normal">
                     <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900">Full Stack Developer...</h5>
-                    <div className="flex items-center justify-between">
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">20 Applicants</p>
+                    <div className="flex items-center justify-between whitespace-nowrap">
+                      <p className="mb-3 font-normal text-gray-700 text-sm">20 Applicants</p>
+                      <p className="mb-3 font-normal text-gray-700 text-sm">Expiry Date: Oct 31, 2023</p>
                     </div>
                   </div>
                 </a>
