e002abc237874a5898876ab76b603d984787ba50
Aderemi Dare <fitzgeraldkachi@gmail.com>
2023-12-02 09:18:53 +0100

    lates on leave management overview

--- src/pages/LeaveManagement/Overview.tsx
+++ src/pages/LeaveManagement/Overview.tsx
@@ -9,12 +9,15 @@ const Overview = () => {
         <DonutChart />
         <div>
 
-          <div className="flex items-center gap-4 bg-white rounded-lg p-4">
-            <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-              <div className="font-medium dark:text-white">
-                <div>Jese Leos</div>
-                <div className="text-sm text-gray-500">Joined in August 2014</div>
+          <div className="flex items-center justify-between gap-4 bg-white rounded-lg p-4">
+            <div>
+              <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+              <div className="font-medium">
+                <div className="text-sm text-gray-500">Jese Leos</div>
+                {/* <div className="text-sm text-gray-500">Joined in August 2014</div> */}
               </div>
+            </div>
+            <span  className="text-sm text-[#0D1227]">23 Leaves</span>
           </div>
 
         </div>
@@ -27,4 +30,4 @@ const Overview = () => {
 
 export default Overview
 
-{/* <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="Base__Svg-sc-ta5t7f-0 icon--stroke"><line x1="1.75" y1="2.75" x2="14.25" y2="2.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line><line x1="1.75" y1="7.75" x2="14.25" y2="7.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line><line x1="1.75" y1="12.75" x2="14.25" y2="12.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line></svg> */}
+{/* <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="Base__Svg-sc-ta5t7f-0 icon--stroke"><line x1="1.75" y1="2.75" x2="14.25" y2="2.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line><line x1="1.75" y1="7.75" x2="14.25" y2="7.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line><line x1="1.75" y1="12.75" x2="14.25" y2="12.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line></svg> */ }
