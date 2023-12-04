dedbdbacf08d71732419b85f857744971b290b0d
Aderemi Dare <fitzgeraldkachi@gmail.com>
2023-12-04 04:42:31 +0100

    main

--- src/components/bottomnav/BottomNav.tsx
+++ src/components/bottomnav/BottomNav.tsx
@@ -14,7 +14,7 @@ const BottomNav = () => {
   ];
 
   return (
-    <section className="bg-[#ffffff] sm:hidden bottomnav px-3 py-2 fixed  bottom-0 w-full shadow-xl flex items-center gap-3 justify-between overflow-x-scroll rounded-tr-3xl rounded-tl-3xl border border-[#aad0d0]">
+    <section className="bg-[#ffffff] sm:hidden px-3 py-2 fixed  bottom-0 w-full shadow-xl flex items-center gap-3 justify-between overflow-x-scroll rounded-tr-3xl rounded-tl-3xl border border-[#aad0d0]">
       {routeList.map((item, index) => {
         const activeItem = location.pathname.includes(item?.route);
         const iconArr = [
@@ -27,7 +27,7 @@ const BottomNav = () => {
             to={item.route}
             key={index}
             className={`${activeItem && "text-white rounded-2xl bg-[#1D8EE6] w-max whitespace-nowrap"
-              } flex justify-evenly flex-1 gap-x-1 items-center rounded-full px-3 py-1 cursor-pointer my-0  text-base space-x-1`}
+              } flex justify-evenly flex-1 gap-x-1 items-center rounded-full px-3 py-2 cursor-pointer my-0  text-base space-x-1`}
           >
             <div className=""> {iconArr[index]}</div>
 
