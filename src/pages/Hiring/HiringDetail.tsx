7ff4753721e68eb4596fbf765af7944011693696
Aderemi Dare <fitzgeraldkachi@gmail.com>
2023-12-14 21:05:56 +0100

    hotest app

--- src/pages/Hiring/HiringDetail.tsx
+++ src/pages/Hiring/HiringDetail.tsx
@@ -5,90 +5,66 @@ import JobDetails from "./JobDetails"
 
 const bread: BreadcrumsbProps[] = [
   {
-      name: 'Home',
-      url: 'dashboard'
+    name: 'Home',
+    url: 'dashboard'
   },
   {
-      name: 'Recruitment & Hiring',
-      url: "employees/overview"
+    name: 'Recruitment & Hiring',
+    url: "employees/overview"
   },
 ]
 const HiringDetail = () => {
   return (
     <div className="h-full pt-3 ">
-            <div className="px-4 md:px-8">
-                <Breadcrumbs {...{ bread }} />
+      <div className="px-4 md:px-8">
+        <Breadcrumbs {...{ bread }} />
 
-                <div className="flex items-center justify-between gap-3">
-                    <div>
-                        <div className="text-lg md:text-2xl">Applications</div>
-                        <div className="text-[#98A1B0] text-[13px] ">Track and manage organization activities. </div>
-                    </div>
-
-                    <div className="flex items-center gap-3">
-                      
-                        <Button className='flex items-center rounded-lg !text-white my-5' prefixIcon={<img src="/plusdash.svg" alt="" />} title="Post New Job" onClick={() => null} />
-                    </div>
-                </div>
-
-                <div className="bg-[#F5F6FA] px-4 md:px-8">
-                    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 py-3 h-max">
-                        <a className="col-span-1 flex flex-col items-start bg-white rounded-md shadow md:flex- md:max-w-xl hover:bg-slate-50 p-4 md:p-5">
-                            <div className="text-lg md:text-4xl text-[rgb(13,18,39)]">234</div>
-                            <div className="flex items-center my-3">
-                                <div className="h-4 w-4 rounded-full bg-[#1D8EE6]"></div>
-                                <span className="text-sm text-[#7C8091] ml-2">Total</span>
-                            </div>
-                        </a>
-                        <a className="col-span-1 flex flex-col items-start bg-white rounded-md shadow md:flex- md:max-w-xl hover:bg-slate-50 p-4 md:p-5">
-                            <div className="text-lg md:text-4xl text-[rgb(13,18,39)]">20</div>
-                            <div className="flex items-center my-3">
-                                <div className="h-4 w-4 rounded-full bg-[#FFB240]"></div>
-                                <span className="text-sm text-[#7C8091] ml-2">Pending</span>
-                            </div>
-                        </a>
-                        <a className="col-span-1 flex flex-col items-start bg-white rounded-md shadow md:flex- md:max-w-xl hover:bg-slate-50 p-4 md:p-5">
-                            <div className="text-lg md:text-4xl text-[rgb(13,18,39)]">160</div>
-                            <div className="flex items-center my-3">
-                                <div className="h-4 w-4 rounded-full bg-[#1EAA79]"></div>
-                                <span className="text-sm text-[#7C8091] ml-2">Approved</span>
-                            </div>
-                        </a>
-                        <a className="col-span-1 flex flex-col items-start bg-white rounded-md shadow md:flex- md:max-w-xl hover:bg-slate-50 p-4 md:p-5">
-                            <div className="text-lg md:text-4xl text-[rgb(13,18,39)]">84</div>
-
-                            <div className="flex items-center my-3">
-                                <div className="h-4 w-4 rounded-full bg-#FF5A4F]"></div>
-                                <span className="text-sm text-[#7C8091] ml-2">Rejected</span>
-                            </div>
-                        </a>
-
-                    </div>
-                </div>
+        <div className="flex items-center justify-between gap-3">
+          <div>
+            <div className="text-lg md:text-2xl whitespace-nowrap flex items-center gap-3 mt-5">Full Stack Developer  <Button className="!bg-[#D1EEE4] !text-[#1EAA79] !p-1 text-[10px]" title="Active" /></div>
+            <div className="text-[#98A1B0] text-sm mt-5">
+              <div className="flex flex-wrap gap-x-3 ga-8 gap-y-1  md:text-x">
+                <p className="mb-1 font-normal text-[#AFB1B6] flex items-center gap-1 md:gap-2"><img src="/briefcase.svg" alt="" /> Part Time (Remote)</p>
+                <p className="mb-1 font-normal text-[#AFB1B6] flex items-center gap-1 md:gap-2"><img src="/graduate.svg" alt="" /> Html, Python, CSS</p>
+                <p className="mb-1 font-normal text-[#AFB1B6] flex items-center gap-1 md:gap-2"><img src="/credit-card-hire.svg" alt="" /> 200k - 350k</p>
+                {/* <p className="mb-1 font-normal text-[#AFB1B6] flex items-center gap-1 md:gap-2"><img src="/credit-card-hire.svg" alt="" />staff</p> */}
+              </div>
             </div>
-            <div className="mt-10  pb-36">
-                <Tabs
-                    data={[
-                        {
-                            label: 'Job Details',
-                            content: (
-                                <>
-                                    <JobDetails />
-                                </>
-                            )
-                        },
-                        {
-                            label: 'ApplicationDetail',
-                            content: (
-                                <>
-                                    <ApplicationDetail />
-                                </>
-                            )
-                        }
-                    ]}
-                />
-            </div>
+
+          </div>
+
+          <div className="flex items-center gap-3">
+
+            <Button className='flex items-center rounded-lg !text-white my-5' prefixIcon={<img src="/edit.svg" alt="" />} title="Edit Post" onClick={() => null} />
+          </div>
         </div>
+      </div>
+      <div className="mt-10  pb-36">
+        <Tabs
+          data={[
+            {
+              label: 'Job Details',
+              content: (
+                <>
+                  <JobDetails />
+                </>
+              )
+            },
+            {
+              label: 'ApplicationDetail',
+              badge: 4,
+              badgeColor: 'red-600',
+              badgeTextColor: 'white',
+              content: (
+                <>
+                  <ApplicationDetail />
+                </>
+              )
+            }
+          ]}
+        />
+      </div>
+    </div>
   )
 }
 
--- src/pages/Hiring/JobDetails.tsx
+++ src/pages/Hiring/JobDetails.tsx
@@ -1,7 +1,63 @@
+import ShareImmg from "./share.svg"
 const JobDetails = () => {
-  return (
-    <div>JobDetails</div>
-  )
+    return (
+        <div className="grid md:grid-cols-5 gap-4">
+            <div className="md:col-span-3">
+                <div className="relative flex items-center bg-[#1E1E1E] text-white bg-['linear-gradient(90deg, #1E1E1E 55.35%, #000 66.4%)] px-3 sm:px-8 py-6 sm:py-10 gap-4 rounded-lg">
+                    <span className="text-2xl md:text-[38.45px]">WE ARE HIRING</span>
+
+                    <img src="/hire-bell.svg" className="absolute right-0 -bottom-1 sm:bottom-0 pr-10 w-24 h-24 sm:w-[unset] sm:h-[unset]" alt="" />
+                </div>
+
+
+
+            <div>
+                <div className="text-lg font-medium my-5">Job Description</div>
+                <div className="text-base my-5">
+                    We are looking for a skilled Full Stack Developer to join our team and help us create and maintain cutting-edge software solutions.
+                </div>
+                <div className="text-lg font-medium">Responsibility</div>
+                <ol className="list-decimal ml-10 text-[#61646B] text-base">
+                    <li>Collaborate with cross-functional teams to understand project requirements and deliver high-quality software solutions.</li>
+                    <li>Design and develop user-friendly and visually appealing front-end applications.</li>
+                    <li>Create responsive web applications that work seamlessly on desktop and mobile devices.</li>
+                    <li>Develop back-end components and server-side logic to ensure optimal system functionality.</li>
+                    <li>Integrate external services and APIs into applications as needed.</li>
+                    <li>Optimize applications for maximum speed and scalability.</li>
+                    <li>Implement security and data protection in applications.</li>
+                    {/* <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li> */}
+                </ol>
+
+                <div className="text-lg font-medium mt-3">Responsibility</div>
+                <ol className="list-decimal ml-10 text-[#61646B] text-base">
+                    <li>Proven experience as a Full Stack Developer or similar role.</li>
+                    <li>Proficiency in front-end technologies, including HTML, CSS, JavaScript, and related frameworks (e.g., React, Angular, or Vue).</li>
+                    <li>Strong experience in back-end technologies, such as Node.js, Python, Ruby, or Java.</li>
+                    <li>Experience with database systems (SQL and NoSQL databases).</li>
+                    <li>Knowledge of web servers and cloud computing platforms.</li>
+                    <li>Familiarity with Git and version control systems.</li>
+                    <li>Strong problem-solving skills and attention to detail.</li>
+                    <li>Excellent communication and teamwork skills.</li>
+                    {/* <li></li>
+                    <li></li>
+                    <li></li> */}
+                </ol>
+                <div className="text-lg font-medium mt-3">Required Skills</div>
+                <div className="text-base text-[#61646B] font-medium mt-3">JavaScript, CSS, React, Angular, Java & any cloud-based programming language will be an additional</div>
+
+
+                <div className="text-lg font-medium mt-3">Share Openings</div>
+                    <img src={ShareImmg} className="mt-3" alt="" />
+            </div>
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default JobDetails
